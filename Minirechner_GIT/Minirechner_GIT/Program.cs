06f72741b3f8c50f4b7f9dd29d31f299b95c27c3
Aaron Delago <02delaar@rgtfo-me.it>
2020-04-21 15:05:02 +0200

    zentrierte Überschrift hinzugefügt

--- Minirechner_GIT/Minirechner_GIT/Program.cs
+++ Minirechner_GIT/Minirechner_GIT/Program.cs
@@ -9,6 +9,33 @@ namespace Minirechner_GIT
     {
         static void Main(string[] args)
         {
+            Titel("Minirechner");
+
+            Console.ReadKey();
         }
-    }
-}
+
+        static void Titel(string Text)
+        {
+            int breite = Console.WindowWidth;
+            int textlaenge = Text.Length;
+            Console.Clear();
+       
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+            for (int i = 0; i < (breite/2)-textlaenge; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(Text);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");   
+            }
+       
+        }
+
+     }
+  }
+
